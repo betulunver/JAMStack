a00188f9e5d038d4dd2c0bf568afdcf955311cd6
Betul UNVER <betul.unver@bilgeadam.com>
2021-08-03 15:19:45 +0300

    jamstack-api created with strapi

---
+++ jamstack-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'add5d90db98fb5b25825afb2900ca3fc'),
+    },
+  },
+});
---
+++ jamstack-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cd6601fa-126d-435a-ba4b-3919bf8b815e'
+};
